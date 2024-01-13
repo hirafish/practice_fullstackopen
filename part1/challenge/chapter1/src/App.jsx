5c89836508ef7891f6c90f39cc6360d57f65dbf8
hirafish <95068237+hirafish@users.noreply.github.com>
2024-01-13 22:43:57 +0900

    part1 challenge1.2

--- part1/challenge/chapter1/src/App.jsx
+++ part1/challenge/chapter1/src/App.jsx
@@ -1,17 +1,9 @@
-const Header = (props) => {
-  return <div>{props.course}</div>;
-};
-
-const Content = (props) => {
+const Part = (props) => {
   return (
-    <div>
+    <p>
       {props.part} {props.exercises}
-    </div>
+    </p>
   );
-};
-
-const Total = (props) => {
-  return <div>Number of exercises {props.total}</div>;
 };
 
 const App = () => {
@@ -25,11 +17,9 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
     </div>
   );
 };
